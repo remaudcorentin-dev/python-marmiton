24a5bcad99699da56b599d3f5bb05b11249848d0
Corentin Remaud <corentinremaud@corentins-macbook-air.home>
2017-06-08 21:29:00 +0200

    made pip installable

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup
+
+setup(
+	name='python-marmiton',
+	version='0.1.2',
+	description='Easy-to-use Python API for the marmiton.com cooking website.',
+	packages=['marmiton'],
+	url='https://github.com/remaudcorentin-dev/python-marmiton',
+	author='Corentin Remaud',
+	author_email='remaudcorentin.dev@gmail.com',
+	license='MIT',
+	zip_safe=False,
+	install_requires=['bs4'],
+	)
