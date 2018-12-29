2ab7813fafb94681331067ec132bfb9029d04511
Corentin Remaud <cremaud@ennov.com>
2018-12-29 16:26:26 +0100

    Update to version 0.3.1 : code optimization + add fields 'nb_comments', 'people_quantity', 'total_time', 'budget', 'difficulty, 'rate', 'author', 'author_tip' & 'tags' + update example script to provide a usage example of all existing fields

--- example.py
+++ example.py
@@ -3,27 +3,37 @@ from marmiton import Marmiton
 
 # Search :
 query_options = {
-  "aqt": "poulet curry",      # Query keywords - separated by a white space
-#  "sort": "rate",             # Sorting options : "markdesc" (rate) | "popularitydesc" (popularity) | "" (empty for relevance, optional)  # Obselete
+  "aqt": "boeuf bourguignon",      # Query keywords - separated by a white space
   "dt": "platprincipal",      # Plate type : "entree", "platprincipal", "accompagnement", "amusegueule", "sauce" (optional)
-  "exp": 3,                   # Plate price : 1 -> Cheap, 2 -> Medium, 3 -> Kind of expensive (optional)
+  "exp": 2,                   # Plate price : 1 -> Cheap, 2 -> Medium, 3 -> Kind of expensive (optional)
   "dif": 2,                   # Recipe difficulty : 1 -> Very easy, 2 -> Easy, 3 -> Medium, 4 -> Advanced (optional)
   "veg": 0,                   # Vegetarien only : 0 -> False, 1 -> True (optional)
 }
 query_result = Marmiton.search(query_options)
 
 # Get :
-recipe = query_result[1]
+recipe = query_result[0]
 main_recipe_url = recipe['url']
 
 detailed_recipe = Marmiton.get(main_recipe_url)  # Get the details of the first returned recipe (most relevant in our case)
 
 # Display result :
-print("## %s, (%s / 5) :" % (detailed_recipe['name'], recipe['rate']))  # Name of the recipe
-print("Cooking Time : %s / Preparation Time : %s" % (detailed_recipe['cook_time'], detailed_recipe['prep_time']))
+print("## %s\n" % detailed_recipe['name'])  # Name of the recipe
+print("Recette par '%s'" % (detailed_recipe['author']))
+print("Noté %s/5 par %s personnes." % (detailed_recipe['rate'], detailed_recipe['nb_comments']))
+print("Temps de cuisson : %s / Temps de préparation : %s / Temps total : %s." % (detailed_recipe['cook_time'] if detailed_recipe['cook_time'] else 'N/A',detailed_recipe['prep_time'], detailed_recipe['total_time']))
+print("Tags : %s" % (", ".join(detailed_recipe['tags'])))
+print("Difficulté : '%s'" % detailed_recipe['difficulty'])
+print("Budget : '%s'" % detailed_recipe['budget'])
 
+print("\nRecette pour %s personne(s) :\n" % detailed_recipe['people_quantity'])
 for ingredient in detailed_recipe['ingredients']:  # List of ingredients
     print("- %s" % ingredient)
 
+print("")
+
 for step in detailed_recipe['steps']:  # List of cooking steps
     print("# %s" % step)
+
+if detailed_recipe['author_tip']:
+	print("\nNote de l'auteur :\n%s" % detailed_recipe['author_tip'])
--- marmiton/__init__.py
+++ marmiton/__init__.py
@@ -7,6 +7,7 @@ import urllib.request
 
 import re
 
+
 class Marmiton(object):
 
 	@staticmethod
@@ -56,50 +57,76 @@ class Marmiton(object):
 		return search_data
 
 	@staticmethod
+	def __clean_text(element):
+		return element.text.replace("\n", "").strip()
+
+	@staticmethod
 	def get(uri):
 		"""
 		'url' from 'search' method.
 		 ex. "/recettes/recette_wraps-de-poulet-et-sauce-au-curry_337319.aspx"
 		"""
+		data = {}
+
 		base_url = "http://www.marmiton.org/"
 		url = base_url + uri
 
 		html_content = urllib.request.urlopen(url).read()
 		soup = BeautifulSoup(html_content, 'html.parser')
 
-
 		main_data = soup.find("div", {"class": "m_content_recette_main"})
 		name = soup.find("h1", {"class", "main-title "}).get_text().strip(' \t\n\r')
 
 		ingredients = [item.text.replace("\n", "").strip() for item in soup.find_all("li", {"class": "recipe-ingredients__list__item"})]
 
+		try:
+			tags = list(set([item.text.replace("\n", "").strip() for item in soup.find('ul', {"class": "mrtn-tags-list"}).find_all('li', {"class": "mrtn-tag"})]))
+		except:
+			tags = []
+
+		recipe_elements = [
+			{"name": "author", "query": soup.find('span', {"class": "recipe-author__name"}) },
+			{"name": "rate", "query": soup.find("span", {"class": "recipe-reviews-list__review__head__infos__rating__value"}) },
+			{"name": "difficulty", "query": soup.find("div", {"class": "recipe-infos__level"}) },
+			{"name": "budget", "query": soup.find("div", {"class": "recipe-infos__budget"}) },
+			{"name": "prep_time", "query": soup.find("span", {"class": "recipe-infos__timmings__value"}) },
+			{"name": "total_time", "query": soup.find("span", {"class": "title-2 recipe-infos__total-time__value"}) },
+			{"name": "people_quantity", "query": soup.find("span", {"class": "title-2 recipe-infos__quantity__value"}) },
+			{"name": "author_tip", "query": soup.find("div", {"class": "recipe-chief-tip mrtn-recipe-bloc "}).find("p", {"class": "mrtn-recipe-bloc__content"}) if soup.find("div", {"class": "recipe-chief-tip mrtn-recipe-bloc "}) else "" },
+		]
+		for recipe_element in recipe_elements:
+			try:
+				data[recipe_element['name']] = Marmiton.__clean_text(recipe_element['query'])
+			except:
+				data[recipe_element['name']] = ""
+
+		try:
+			cook_time = Marmiton.__clean_text(soup.find("div", {"class": "recipe-infos__timmings__cooking"}).find("span"))
+		except:
+			cook_time = "0"
+
+		try:
+			nb_comments = Marmiton.__clean_text(soup.find("span", {"class": "recipe-infos-users__value mrtn-hide-on-print"})).split(" ")[0]
+		except:
+			nb_comments = ""
+
 		steps = []
 		soup_steps = soup.find_all("li", {"class": "recipe-preparation__list__item"})
 		for soup_step in soup_steps:
 			soup_step.find("h3").decompose()
-			steps.append(soup_step.text.replace("\n", "").replace("\t", "").strip())
-
-		prep_time = "0"
-		cook_time = "0"
-
-		try:
-			prep_time = soup.find("span", {"class": "recipe-infos__timmings__value"}).text.replace("\n", "").replace("\t", "").strip()
-		except:
-			pass
-
-		try:
-			cook_time = soup.find("div", {"class": "recipe-infos__timmings__cooking"}).find("span").text.replace("\n", "").replace("\t", "").strip()
-		except:
-			pass
+			steps.append(Marmiton.__clean_text(soup_step))
 
 		image = soup.find("img", {"id": "af-diapo-desktop-0_img"})['src'] if soup.find("img", {"id": "af-diapo-desktop-0_img"}) else ""
 
-		data = {"ingredients": ingredients,
-				"steps": steps,
-				"name": name,
-				"image": image if image else "",
-				"prep_time": prep_time,
-				"cook_time": cook_time}
+		data.update({
+			"ingredients": ingredients,
+			"steps": steps,
+			"name": name,
+			"tags": tags,
+			"image": image if image else "",
+			"nb_comments": nb_comments,
+			"cook_time": cook_time
+		})
 
 		return data
 
