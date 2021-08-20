b1515819d3eb4062a7f8d3bbe475e18e80321f24
Paul Paillet <54935012+PaulPaillet@users.noreply.github.com>
2021-08-21 01:50:36 +0200

    Update example.py after fixing parsing issue
    This example prints all titles of the recipes matching the options.

--- example.py
+++ example.py
@@ -11,28 +11,6 @@ query_options = {
 query_result = Marmiton.search(query_options)
 
 # Get :
-recipe = query_result[0]
-main_recipe_url = recipe['url']
-
-detailed_recipe = Marmiton.get(main_recipe_url)  # Get the details of the first returned recipe (most relevant in our case)
-
-# Display result :
-print("## %s\n" % detailed_recipe['name'])  # Name of the recipe
-print("Recette par '%s'" % (detailed_recipe['author']))
-print("Noté %s/5 par %s personnes." % (detailed_recipe['rate'], detailed_recipe['nb_comments']))
-print("Temps de cuisson : %s / Temps de préparation : %s / Temps total : %s." % (detailed_recipe['cook_time'] if detailed_recipe['cook_time'] else 'N/A',detailed_recipe['prep_time'], detailed_recipe['total_time']))
-print("Tags : %s" % (", ".join(detailed_recipe['tags'])))
-print("Difficulté : '%s'" % detailed_recipe['difficulty'])
-print("Budget : '%s'" % detailed_recipe['budget'])
-
-print("\nRecette pour %s personne(s) :\n" % detailed_recipe['people_quantity'])
-for ingredient in detailed_recipe['ingredients']:  # List of ingredients
-    print("- %s" % ingredient)
-
-print("")
-
-for step in detailed_recipe['steps']:  # List of cooking steps
-    print("# %s" % step)
-
-if detailed_recipe['author_tip']:
-	print("\nNote de l'auteur :\n%s" % detailed_recipe['author_tip'])
+recipes = query_result['props']['pageProps']['searchResults']['hits']
+for recipe in recipes:
+	print(recipe['title'])
